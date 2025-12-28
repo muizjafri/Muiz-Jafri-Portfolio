dbe6db2aecae1de7a4690ed6980a965475a9de83
Muiz Jafri <muizjafri1376@gmail.com>
2025-12-28 00:41:07 -0500

    portfolio

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import github from './assets/githublogo.png'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://github.com/muizjafri" target="_blank">
+          <img src={github} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          subscript
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
